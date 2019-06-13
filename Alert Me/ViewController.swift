941e19970d131897f81b80df845412fa89bf91e8
Mina Habib <mina_habib@hotmail.com>
2019-06-12 21:58:13 -0400

    Initial Commit

---
+++ Alert Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alert Me
+//
+//  Created by Mina Habib on 6/12/19.
+//  Copyright © 2019 Mina Habib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Alert MeTests/Alert_MeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Alert_MeTests.swift
+//  Alert MeTests
+//
+//  Created by Mina Habib on 6/12/19.
+//  Copyright © 2019 Mina Habib. All rights reserved.
+//
+
+import XCTest
+@testable import Alert_Me
+
+class Alert_MeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
